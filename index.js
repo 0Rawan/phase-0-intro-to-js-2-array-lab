8de221b8100651a758454579e9fd42941cdda6e8
Rawan <r.k096@hotmail.com>
2024-02-21 22:01:58 +0300

    done

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const cats = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendCat(name){
     cats.push(name)
 }
+
 function destructivelyPrependCat(name) {
     cats.unshift(name)
 }
+
 function destructivelyRemoveFirstCat(){
     cats.shift()
 }
+
 function destructivelyRemoveLastCat(){
         cats.pop()
 }
+
 function appendCat(name){
     const arr = [...cats]
     arr.push(name)
